71c1ff3d36cf068f4fccdb151dc5db853b3cc950
thaitv21 <thaitv210@gmail.com>
2022-01-21 11:47:26 +0700

    Ignore eslint

--- repositories/ModelNameRepositoryImpl.ts
+++ repositories/ModelNameRepositoryImpl.ts
@@ -4,21 +4,25 @@ import { ModelNameRepository } from './interfaces/ModelNameRepository';
 export default class ModelNameRepositoryImpl implements ModelNameRepository {
   list = async () => {
     throw new Error('Method not implemented!');
-  }
+  };
 
   get = async (id: string | number) => {
+    console.log(id);
     throw new Error('Method not implemented!');
-  }
+  };
 
   create = async (modelName: ModelName) => {
+    console.log(modelName);
     throw new Error('Method not implemented!');
-  }
+  };
 
   update = async (id: string | number, modelName: ModelName) => {
+    console.log(id, modelName);
     throw new Error('Method not implemented!');
-  }
+  };
 
   delete = async (id: string | number) => {
+    console.log(id);
     throw new Error('Method not implemented!');
-  }
+  };
 }
